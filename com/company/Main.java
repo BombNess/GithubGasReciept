a181e17b93863efe9a09fa7f7ddad29c39d271ea
levi <levi.rappleye.2023@student.lhschools.org>
2020-02-19 13:01:12 -0500

    Intellij Commit

---
+++ com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Main{
+
+    public static void main(String[] args){
+        System.out.println("+----------------------+");
+        System.out.println("|      Speedway        |");
+        System.out.println("|                      |");
+        System.out.println("|    6312 E Main St    |");
+        System.out.println("|    7881 E Main St    |");
+        System.out.println("|     7860 E Broad St  |");
+        System.out.println("+----------------------+");
+        System.out.println("|    Date       time   |");
+        System.out.println("|   1/13/20     9:01 pm|");
+        System.out.println("|Invoice#       299090 |");
+        System.out.println("|Auth#                 |");
+        System.out.println("|      Mastercard      |");
+        System.out.println("|    Account Number    |");
+        System.out.println("|    ************97    |");
+        System.out.println("|  PUMP  PRODUCT  $/G  |");
+        System.out.println("|   02   DIESEL   3.79 |");
+        System.out.println("|                      |");
+        System.out.println("|        Total         |");
+        System.out.println("|       $90.96         |");
+        System.out.println("+----------------------+");
+        System.out.println("|   Thank You For      |");
+        System.out.println("|     Your Purchase!   |");
+        System.out.println("+----------------------+");
+
+
+    }
+}
